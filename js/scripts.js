07aced90f3de3f0040383e586062842acc43d49e
r-agenor <roseagenor@gmail.com>
2025-01-13 22:40:23 -0500

    JavaScript Updates
    Addition of hamburger icon to mobile screen navigation menu
    + of Javascript to add interactivity to hamburger menu
    + to HTML files to add icon kits and to incoporate JS code

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 console.log("Hello! Welcome to my Portfolio")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+      } 
+
+      else {
+        x.className = 'navtoggle';
+      }
+  }
